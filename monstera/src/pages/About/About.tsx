ee2bc91e26b22df3c490122a3686cb571299e194
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2023-05-03 00:07:05 +0200

    about section

--- monstera/src/pages/About/About.tsx
+++ monstera/src/pages/About/About.tsx
@@ -5,6 +5,13 @@ import SpotifyPlayer from '../../components/SpotifyPlayer/SpotifyPlayer'
 const About: React.FC = () => {
   return (
     <div className='about'>
+      <div className="pics">
+      <img src="monstera.jpg" alt="" />
+      </div>
+      <div className="bio">
+        <p>Trumpeter and composer Ola Lømo Ellingsen has strong references to club, electronic and jazz music. With his sextet MONSTERA he presents a blend of danceable grooves, resilient melodies and cosmetic soundscapes or “Dancefloor-jazz”.</p>
+
+      </div>
       <SpotifyPlayer />
     </div>
   )
--- monstera/src/pages/Home/Home.tsx
+++ monstera/src/pages/Home/Home.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import SpotifyPlayer from '../../components/SpotifyPlayer/SpotifyPlayer'
 import './Home.css'
 
 const Home: React.FC = () => {
   return (
     <div className='home'>
+      <p>our debut album "monstera" is out now!</p>
        <a className='cover' href="https://orcd.co/album-monstera">
         <img src="cover.png" alt="Album cover" />
       </a>
