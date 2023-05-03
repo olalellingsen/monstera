75470a8fef7f86039eb36eab1cc26242ac888dac
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2023-05-03 12:18:07 +0200

    icon

--- monstera/src/pages/Home/Home.tsx
+++ monstera/src/pages/Home/Home.tsx
@@ -4,6 +4,11 @@ import './Home.css'
 const Home: React.FC = () => {
   return (
     <div className='home'>
+      {/* <div className="icons">
+        <a href="">
+          <img src="insta-icon.png" alt="" height={40}/>
+        </a>
+      </div> */}
       <p>our debut album "monstera" is out now!</p>
        <a className='cover' href="https://orcd.co/album-monstera">
         <img src="cover-min.png" alt="Album cover" />
